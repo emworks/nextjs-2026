fd60bf9051d1cf12f8fad59a37de81aa25de3f92
Yevgeny Yakushev <eviakushev@edu.hse.ru>
2026-02-23 20:15:45 +0300

    clean up

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,3 @@
-import { FC } from "react";
 import Layout from "@/components/Layout";
 
-const RootLayout: FC<LayoutProps<"/">> = ({ children }) => {
-  return <Layout>{children}</Layout>;
-}
-
-export default RootLayout;
+export default Layout;
